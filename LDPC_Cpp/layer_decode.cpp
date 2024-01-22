3583810501eccb44d492cd922d4f8d5dbcf813bd
ashuthosh-bharadwaj <ashuthosh.bharadwaj@research.iiit.ac.in>
2024-01-22 19:09:46 +0530

    layered decoder cpp

--- LDPC_Cpp/layer_decode.cpp
+++ LDPC_Cpp/layer_decode.cpp
@@ -8,7 +8,7 @@ using matlab::mex::ArgumentList;
 
 class MexFunction : public matlab::mex::Function {//Base class for C++ MEX functions
 public:
-    void operator()(ArgumentList outputs, ArgumentList inputs) {//Container for inputs and outputs from MEX
+    void operator()(ArgumentList outputs, ArgumentList inputs){//Container for inputs and outputs from MEX
     ArrayFactory factory; //to create output arrays.
 
     TypedArray<double> llr_out = inputs[0];
@@ -17,29 +17,23 @@ public:
 
     int numIters = inputs[3][0];
     int where;
-    const CellArray BitsinCheck = inputs[4]; 
-    const CellArray LLR_registers = inputs[5];
+    CellArray BitsinCheck = inputs[4]; 
+    CellArray LLR_registers = inputs[5];
 
     int m = LDPC_details[0];
     int z = LDPC_details[1];
 
-    TypedArray<double> some = BitsinCheck[0];
-
-    std::cout<<"Hello MEX here!"<<std::endl;
-    std::cout<<"input[0] is llr_out and its size: "<<llr_out[0]<<std::endl;
-    std::cout<<"input[1] is codeword and its size: "<<codeword[0]<<std::endl;
-    std::cout<<"input[2] is LDPC_details and its size: "<<(sizeof(LDPC_details))<<std::endl;
-    std::cout<<"input[3] is numIters and its size: "<<(sizeof(numIters))<<std::endl;
-    std::cout<<"input[4] is BitsinCheck and the first of its size: "<<some[0]<<std::endl;
-
-    // std::cout<<"input[5] is LLR_registers and its size: "<<(sizeof())<<std::endl;
-
+    // std::cout<<"Hello Mex"<<std::endl;
 
     auto r = llr_out; // intermediate llrs
+    int anti_matches;
     bool decode_result; // Did the decoder get to the codeword?
+
+    int num_llrs = llr_out.getNumberOfElements();
 
     for (int iter=0; iter < numIters; iter++)
     {
+
         for (int layer=0; layer < m; layer++)
         {
             
@@ -60,30 +54,111 @@ public:
             for (int row_num=0; row_num < z; row_num++)
             {
                 where = layer*z + row_num;
+                
                 // init 
-                // TypedArray<double> var_set = BitsinCheck[where];
-                // TypedArray<double> old_llrs = LLR_registers[where];
+                TypedArray<double> var_set = BitsinCheck[where];
+                TypedArrayRef<double> old_llrs = LLR_registers[where];
+
                 int num_vars = var_set.getNumberOfElements();
+
+                // Self Belief Removal
+                for(int j=0; j < num_vars; j++)
+                {
+                    old_llrs[j] = llr_out[var_set[j]-1] - old_llrs[j];     
+                }
+                
+                double temp[num_vars];
+                double min1 = 1000.0;
+                double min2 = 1000.0;
+
+                int min1_idx, parity = 1;
+                double val;
+
+                // Min operation over llr register entries
+                for(int j=0; j < num_vars; j++)
+                {
+                    val = std::fabs(old_llrs[j]);
+
+                    if(val < min2)
+                    {
+                        if (val < min1)
+                        {
+                            min2 = min1;
+                            min1 = val;
+                            min1_idx = j;
+                        }
+                        else
+                        {
+                            min2 = val;
+                        }
+
+                    }
+                    parity *= sign_fn(old_llrs[j]);
+                }
 
                 for(int j=0; j < num_vars; j++)
                 {
-                    old_llrs
+                    temp[j] = min1;
+
+                    if(j == min1_idx)
+                    {
+                        temp[j] = min2;
+                    }              
+
+                    temp[j] *= (sign_fn(old_llrs[j])*parity);
                 }
 
-            }   
-    //             LLR_registers{(layer-1)*z + row_num} = llr_out(BitsinCheck{(layer-1)*z + row_num})' - LLR_registers{(layer-1)*z + row_num};
-    //             LLR_registers{(layer-1)*z + row_num} = Min(LLR_registers{(layer-1)*z + row_num});
-    //             r(BitsinCheck{(layer-1)*z + row_num}) = r(BitsinCheck{(layer-1)*z + row_num}) + LLR_registers{(layer-1)*z + row_num}';
-    //         end              
+                for(int j=0; j < num_vars; j++)
+                {
+                    old_llrs[j] = temp[j];
+                    r[var_set[j]-1] += old_llrs[j];
+                }
 
-            
+            }
+
+            for(int i = 0; i < num_llrs; i++)
+            {
+                llr_out[i] = r[i];
+            }
 
         }
-    }
-        
-    // output[0] = factory.CreateScalar(decode_result);
+
+        anti_matches = 0;
+
+        for(int i = 0; i < num_llrs; i++){if((r[i] < 0) != codeword[i]){anti_matches++;}}
+
+        if (anti_matches == 0)
+        {   
+            decode_result = true;
+            break;
+        }
 
     }
+
+    if (anti_matches == 0)
+    {   
+        decode_result = true;
+    }
+    else
+    {
+        decode_result = false;
+    }
+
+    outputs[0] = factory.createScalar(decode_result);
+    }
+
+    // Sign function such that (-inf, 0) -> -1, [0, inf) -> 1    
+    int sign_fn(double x){
+        if(x < 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
 };
 
 
@@ -111,3 +186,50 @@ public:
     //     end
 
     // end
+
+
+
+/*Extra code bits for help:
+
+    // TypedArray<double> const newArray = BitsinCheck[0][0];
+    // for (auto e : some) 
+    // {
+    //     std::cout << e << std::endl;
+    // } 
+
+    // std::cout<<"input[5] is LLR_registers and the first of its size: "<<LLR_registers[0]<<std::endl;
+
+    // TypedArray<double> old_llrs = LLR_registers[0];
+    // for (auto u: old_llrs)
+    // {
+    //     u += 5;
+    // }
+
+
+
+    // int nn = LLR_registers[0].getNumberOfElements();
+    // std::cout<<"Num elements in LLR_registers"<<nn<<std::endl;
+
+    // for (int jj = 0; jj < nn; jj++)
+    // {   
+    //     LLR_registers[jj][0] += 5;
+    // }
+
+    // TypedArray<double> old_llrs2 = LLR_registers[0];
+    
+    // for (auto u: old_llrs2)
+    // {
+    //     std::cout<<u<<", ";
+    // }
+
+    // TypedArrayRef<double> doubleArray = BitsinCheck[1][0];
+
+
+
+
+    std::cout<<"temp array after min() operation over old_llrs"<<std::endl;
+    for(auto elem: temp)
+    {
+        std::cout<<elem<<std::endl;
+    }
+*/
--- LDPC_Matlab/ldpc_check.m
+++ LDPC_Matlab/ldpc_check.m
@@ -1,13 +1,13 @@
 setup;
 
-%SNRdB = [-2:1:0, 0.25:0.25:3, 4:6];
-% SNRdB = 1.75:0.25:3.5;
-SNRdB = -2;
+% SNRdB = [-2:1:0, 0.25:0.25:3, 4:6];
+SNRdB = 1.75:0.25:3.5;
+% SNRdB = -2;
 
 snr_len = numel(SNRdB);
 numIters = 20;
 P_ecw = zeros(1, snr_len);
-numTrials = 1;
+numTrials = 1000;
 
 tic 
 for snr_idx = 1:snr_len
@@ -17,8 +17,6 @@ for snr_idx = 1:snr_len
     N_errors = 0;
 
     for trial = 1:numTrials
-        
-        LLR_registers = LLR_registers_init;
 
         message = randi([0,1],1,msg_len)';
         codeword = ldpcEncode(message, Encode_config);
@@ -35,13 +33,14 @@ for snr_idx = 1:snr_len
         
         LDPC_details = [numSubMatrixRows, z];
 
-        layer_decode(llr_out, logical(codeword), uint8(LDPC_details), numIters, BitsinCheck, LLR_registers);
-
-        % N_errors = N_errors + decode_result;
+        decode_result = layer_decode(llr_out, logical(codeword), uint8(LDPC_details), numIters, BitsinCheck, LLR_registers);
+        N_errors = N_errors + (~decode_result);
     end
 
-    P_ecw(snr_idx) = N_errors;
-        
+    P_ecw(snr_idx) = N_errors   
 
 end
 toc
+
+P_ecw = P_ecw/numTrials;
+save('./Output/Layered_newcheck.mat');
