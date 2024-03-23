e0152836d46016415a679eb598aada3c784575da
ashuthosh-bharadwaj <ashuthosh.bharadwaj@research.iiit.ac.in>
2024-03-23 15:25:29 +0530

    reldec

--- RELDEC.m
+++ RELDEC.m
@@ -1,29 +1,49 @@
-load('Datasets/LC_dataset.mat');
-load('Datasets/LDPC_init.mat');
+load('LDPC_Matlab/Datasets/LC_dataset.mat');
+load('LDPC_Matlab/Datasets/LDPC_init.mat');
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 max_la = 0;
 for i=1:num_clusters
     max_la = max(max_la, numel(vns_in_cluster{i,1}));
 end
 
 z = numel(clusters{1});
-tau = ceil(numSubMatrixRows/Z);
+tau = num_clusters;
+
+if tau == ceil(numSubMatrixRows/Z)
+    disp("Sanity checked");
+end 
+
+ldpc_registers = cell(tau,1);
+
+for i = 1:tau
+    
+    CNs = clusters{i};
+    VNs = vns_in_cluster{i};
+    
+    num_vns = numel(CNs);
+    num_cns = numel(VNs);
+    
+    ldpc_registers{i} = zeros(num_cns, num_vns);
+
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 l_max = 50;
 epsilon = 0.7;
 alpha = 0.1;
 beta = 0.9;
 
-A = 0:1:tau;
-Q = zeros(tau,2^max_la);
+A = 1:tau;
+num_states = 2^max_la;
+Q = zeros(tau,num_states);
 
 
 load('./LDPC_Matlab/L_dataset.mat');
 load('./LDPC_Matlab/C_dataset.mat');
 
-ndata = size(L);
-ndata = ndata(1);
+[ndata, ~] = size(L);
+
 
 S = zeros(num_clusters,1);
 
@@ -44,9 +64,13 @@ for idx = 1:ndata
         if rand <= epsilon
             a = randi(tau,1);
         else
-            Valid_states = Q(:, S);
-            [~,a] = max(Valid_states(:));
-            
+            QQ = zeros(tau,1);
+            for i = A
+                QQ(i) = Q(A(i), S(i));
+            end
+
+            [~,a] = max(QQ);
+            a = A(a);
         end
         
         local_flood; %(tanh first and sum second)
@@ -62,6 +86,5 @@ for idx = 1:ndata
         S(a) = s_a;
 
     end
-
 
 end
--- local_flood.m
+++ local_flood.m
@@ -1,2 +1,39 @@
 % Based on given cluster, find the right set of var nodes and checknodes and commence flooding; 
-clust = clusters{a};
+CNs = clusters{a};
+VNs = vns_in_cluster{a};
+
+num_cns = numel(CNs);
+num_vns = numel(VNs);
+
+idxmap_C = dictionary(1:num_cns, CNs);
+idxmap_V = dictionary(VNs, 1:num_vns);
+
+
+current = ldpc_registers{a};
+
+
+% C -> V
+
+for cn_idx = 1:num_cns
+
+    places = BitsinCheck{idxmap_C(cn_idx)};
+    current(cn_idx, idxmap_V(places)) = l(places) -  current(cn_idx, idxmap_V(places));
+
+    Mforw =  tanh(current(cn_idx, idxmap_V(places))/2) ;
+    product = prod(Mforw);
+    Mforw = 2*atanh(product./Mforw);
+    
+    current(cn_idx, idxmap_V(places)) = Mforw;
+
+end
+
+
+% V -> C
+
+l(VNs) = l(VNs) + sum(current);
+
+
+
+
+
+ldpc_registers{a} = current; % re-init
